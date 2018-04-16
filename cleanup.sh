27e60e708666d0021cce197f0d720108c8a0c6b2
Seen.Fan <seen.ys.fan@fii-foxconn.com>
2018-04-16 06:25:52 -0400

    Modify clean up script

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-lvremove /dev/instance-vg/instance-lv
-vgremove instance-vg
-vgremove image-vg
+lvremove /dev/instance_vg/instance_lv
+vgremove instance_vg
+vgremove image_vg
 pvremove /dev/sda1
 pvremove /dev/sda2
 vgreduce cinder-volumes /dev/sdb
@@ -14,6 +14,10 @@ d
 
 w
 EOF
-
-
+umount /var/lib/glance/images
+rm -r -f /var/lib/glance/images
+mv /var/lib/glance/images_1 /var/lib/glance/images
+umount /var/lib/nova/instances
+rm -r -f /var/lib/nova/instances
+mv /var/lib/nova/instances_1 /var/lib/nova/instances
 
