ee6e88b9816cda3b6efc0e3e1812422149d6c8a6
Seen.Fan <seen.ys.fan@fii-foxconn.com>
2018-04-16 06:25:33 -0400

    Using variables to replace vg,lv and mount points

--- setup_lvm.pp
+++ setup_lvm.pp
@@ -31,7 +31,32 @@ define add_hdd_to_vg(
   }
 }
 
+define append_if_no_such_line($file, $line){
+  exec { "echo '$line' >> '$file'":
+    path        => ['/bin','/usr/bin','/sbin','/usr/sbin'],
+    unless => "grep -Fx '$line' '$file'",
+  }
+}
 
+define move_folder($old_pos, $new_pos){
+  exec { "mv $old_pos $new_pos":
+    path        => ['/bin','/usr/bin','/sbin','/usr/sbin'],
+    unless => "test -d $new_pos",
+  }
+}
+
+
+define new_folder(
+  $path,
+  $user,
+  $group,
+  $type
+){
+  exec { "mkdir $path && chown $user:$group $path && chcon -u system_u -r object_r -t $type -l s0 $path":
+    path        => ['/bin','/usr/bin','/sbin','/usr/sbin'],
+    unless => "test -d $path",
+  }
+}
 
 class setup_lvm{
 
@@ -58,10 +83,22 @@ class setup_lvm{
           logoutput => true,
         }
         $hdd1 = $hddlist[0]
-        setup_vg{"instance": vg_name => "instance-vg", dev_path => "${hdd1}1"}
-        setup_lv{"instance-lv": vg_name => "instance-vg", lv_name => "instance-lv"}
-        setup_vg{"image": vg_name => "image-vg", dev_path => "${hdd1}2"}
-        setup_lv{"image-lv": vg_name => "image-vg", lv_name => "image-lv"}
+        $instance_vg_name = "instance_vg"
+        $instance_lv_name = "instance_lv"
+        $instance_mount_point = "/var/lib/nova/instances"
+        setup_vg{"instance": vg_name => "${instance_vg_name}", dev_path => "${hdd1}1"}
+        setup_lv{"instance-lv": vg_name => "${instance_vg_name}", lv_name => $instance_lv_name}
+	append_if_no_such_line{"instance-lv": file => "/etc/fstab", line => "/dev/mapper/${instance_vg_name}-${instance_lv_name}	${instance_mount_point}  xfs     defaults        0       0"}
+        move_folder{"instance-lv": old_pos => "${instance_mount_point}", new_pos => "${instance_mount_point}_1"}
+        new_folder{"instance-lv": path => "${instance_mount_point}", user => "nova", group => "nova", type => "nova_var_lib_t"}
+        $image_vg_name = "image_vg"
+        $image_lv_name = "image_lv"
+        $image_mount_point = "/var/lib/glance/images"
+        setup_vg{"image": vg_name => "${image_vg_name}", dev_path => "${hdd1}2"}
+        setup_lv{"image-lv": vg_name => "${image_vg_name}", lv_name => "image-lv"}
+	append_if_no_such_line{"image-lv": file => "/etc/fstab", line => "/dev/mapper/${image_vg_name}-${image_lv_name}	${image_mount_point}  xfs     defaults        0       0"}
+        move_folder{"image-lv": old_pos => "${image_mount_point}", new_pos => "${image_mount_point}_1"}
+        new_folder{"image-lv": path => "${image_mount_point}", user => "glance", group => "glance", type => "glance_var_lib_t"}
         add_hdd_to_vg{"volume": vg_name => "cinder-volumes", dev_path => "${hddlist[1]}" }
       }
     8: {}
