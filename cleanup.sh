3163adce56453ae266980b7783f2e6645fe2c4ac
Seen.Fan <seen.ys.fan@fii-foxconn.com>
2018-04-16 21:15:48 -0400

    Unmount and wipe fs before remove vg,lv

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+umount /var/lib/glance/images
+umount /var/lib/nova/instances
+wipefs -af /dev/instance_vg/instance_lv
+wipefs -af /dev/image_vg/image_lv
 lvremove /dev/instance_vg/instance_lv
+lvremove /dev/image_vg/image_lv
 vgremove instance_vg
 vgremove image_vg
 pvremove /dev/sda1
@@ -14,10 +19,8 @@ d
 
 w
 EOF
-umount /var/lib/glance/images
 rm -r -f /var/lib/glance/images
 mv /var/lib/glance/images_1 /var/lib/glance/images
-umount /var/lib/nova/instances
 rm -r -f /var/lib/nova/instances
 mv /var/lib/nova/instances_1 /var/lib/nova/instances
 
